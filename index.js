981068aa6b856bbd91746fd591e1522b529e071d
Corentin Hanscotte <corentin.hanscotte@epitech.eu>
2024-06-01 15:54:42 +0200

    delete comment, update text

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ $(".image").click(function () {
 
 /*DARK MODE*/
 function switchDark() {
-    let className = document.querySelector('html').className;
-    document.querySelector('html').className = className === 'dark' ? '' : 'dark';
+    let htmlClassName = document.querySelector('html').className;
+    document.querySelector('html').className = htmlClassName === 'dark' ? '' : 'dark';
 }
 
 /*MENU*/
@@ -15,17 +15,3 @@ function Menu(e) {
     let list = document.querySelector('ul');
     e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]') , list.classList.add('opacity-100'), list.classList.remove('absolute')) : (e.name = "menu" , list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'), list.classList.add('absolute'))
 }
-
-/*ANIMATIONS*/
-/*let myText = 'Bienvenue sur mon portfolio!';
-let myArray = myText.split("");
-let loopTimer;
-
-function frameLooper() {
-    if (myArray.length > 0) {
-        document.getElementById("bienvenue").innerHTML += myArray.shift();
-    }
-    loopTimer = setTimeout('frameLooper()', 70);
-}
-
-frameLooper();*/
